60a42ba69688ca9c25b6aab99997fdd3017da0e0
FatihGrsoy <fthgrsoy@hotmail.com>
2022-09-24 17:54:34 +0300

    Java Odevi

---
+++ inheritanceDemo/inheritanceDemo/src/AskerKredisiManager.java
@@ -0,0 +1,3 @@
+public class AskerKredisiManager extends BaseKrediManager{
+
+}
---
+++ inheritanceDemo/inheritanceDemo/src/KrediUI.java
@@ -0,0 +1,8 @@
+public class KrediUI {
+//    public void KrediHesapla(OgretmenKrediManager ogretmenKrediManager){
+//        ogretmenKrediManager.Hesapla();
+//    }
+    public void KrediHesapla(BaseKrediManager baseKrediManager){
+        baseKrediManager.Hesapla();
+    }
+}
